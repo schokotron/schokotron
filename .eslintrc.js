a9aeeae308666f81acb8674ec539d4a44b940253
David Beermann <mail@davidbeermann.com>
2018-03-01 17:43:14 +0100

    Added Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'standard',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  env: {
+    // browser: true,
+    // node: true,
+    // jest: true,
+  },
+}
